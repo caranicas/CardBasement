d6863935c01affcf171b88905c147fa2bc940647
caranicas <kendrick.caranicas@gmail.com>
2026-03-09 15:51:05 -0400

    basics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,19 +5,29 @@ import "../App.css";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="nav">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-        <Link to="/gallery" className="[&.active]:font-bold">
-          Gallery
-        </Link>
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+      <div className="app-shell">
+        <header className="site-header">
+          <nav className="nav">
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>
+            <Link to="/gallery" className="[&.active]:font-bold">
+              Gallery
+            </Link>
+            <Link to="/about" className="[&.active]:font-bold">
+              About
+            </Link>
+          </nav>
+        </header>
+
+        <main className="page-main">
+          <Outlet />
+        </main>
+
+        <footer className="site-footer">
+          <p>Card Basement</p>
+        </footer>
       </div>
-      <hr />
-      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
